b53affc4f65b31872ea5b09c8b309fcf91eca2b4
Vitaliy Katkalo <katcalo.v@gmail.com>
2019-09-24 22:40:22 +0300

    lessons terraform v-1.0

---
+++ lessons-1/lessons-1.tf
@@ -0,0 +1,12 @@
+provider "aws" {}
+
+resource "aws_instance" "my_Ubuntu" {
+  count         = 1                       #создаеться 1 host
+  ami           = "ami-0ac05733838eabc06" #тип os
+  instance_type = "t2.micro"
+  tags = {
+    Name    = "My Amazon Server"
+    Owner   = "Vitaliy Katkalo"
+    Project = "Terraform Lessons"
+  }
+}
